8d42d5a07a0a2ecfc61c19af58700da85ba5cfd9
hosung-222 <gik11kr@naver.com>
2023-11-10 19:33:58 +0900

    refactor : 계산 기능 분리

--- src/main/java/christmas/domain/Events/DdayEvent.java
+++ src/main/java/christmas/domain/Events/DdayEvent.java
@@ -9,13 +9,17 @@ public class DdayEvent {
     private static final int DISCOUNT_START_POINT = 1000;
     private static final int DISCOUNT_AMOUNT = 100;
 
+    private int getDiscountAmount(int date){
+        return DISCOUNT_START_POINT + DISCOUNT_AMOUNT * (date-EVENT_START_DAY);
+    }
+
     public boolean validateDate(int date){
         return EVENT_START_DAY <= date && date <= EVENT_END_DAY;
     }
 
     public void getDdayDiscount(int date , TotalEvent totalEvent){
         if(validateDate(date)){
-            int dDayDiscountAmount = DISCOUNT_START_POINT + DISCOUNT_AMOUNT * (date-EVENT_START_DAY);
+            int dDayDiscountAmount = getDiscountAmount(date);
             totalEvent.updateBenefitHistory(PRESENT_EVENT_TITLE, dDayDiscountAmount);
         }
     }
