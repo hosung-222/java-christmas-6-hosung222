e627a6b0a0fd4a369ca07613fbab4e0062d06aa8
hosung-222 <gik11kr@naver.com>
2023-11-10 19:52:55 +0900

    feat : 메뉴 기능 구현

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,25 @@
+package christmas.domain;
+
+public class Menu {
+    private String name;
+    private int price;
+    private String category;
+
+    public Menu(String name, int price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
