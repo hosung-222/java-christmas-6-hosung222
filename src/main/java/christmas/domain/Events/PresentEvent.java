8d15ec01d70a7665e72c7fb5c9572ec559e10d28
hosung-222 <gik11kr@naver.com>
2023-11-12 15:26:10 +0900

    feat : 총 혜택 금액 출력 기능 구현
    총 할인 받은 금액의 총합 출력 기능 만약 할인 받은 금액이 없다면 없음 출력

--- src/main/java/christmas/domain/Events/PresentEvent.java
+++ src/main/java/christmas/domain/Events/PresentEvent.java
@@ -6,15 +6,16 @@ import java.util.Map;
 
 public class PresentEvent {
     private static final String EVENT_TITLE = "증정 이벤트";
+    private static final String SPACE = " ";
+    private static final String NEXT_LINE = "\n";
+    private static final String COUNT_STRING = "개";
+    private static final String NON_VALUE = "없음";
     private static final int MINIMUM_AMOUNT_FOR_PRESENT = 120000;
     private static final int CHAMPAGNE_PRESENT_PRICE = 25000;
     private static final String CHAMPAGNE_PRESENT_NAME = "샴페인";
     private static final int CHAMPAGNE_PRESENT_AMOUNT = 1;
     private final Map<String, Integer> present = new HashMap<>();   // Map < 증정 선물 이름 : 수량 >
 
-    public PresentEvent() {
-        present.put(CHAMPAGNE_PRESENT_NAME, CHAMPAGNE_PRESENT_AMOUNT);
-    }
 
     public boolean isPresentEventApplicable(int totalAmount) {
         return totalAmount > MINIMUM_AMOUNT_FOR_PRESENT;
@@ -23,6 +24,7 @@ public class PresentEvent {
     public Map<String, Integer> getPresentEvent(int totalAmount, TotalEvent totalEvent) {
         Map<String, Integer> presentEventResult = new HashMap<>();
         if (isPresentEventApplicable(totalAmount)) {
+            present.put(CHAMPAGNE_PRESENT_NAME, CHAMPAGNE_PRESENT_AMOUNT);
             //<증정 메뉴> return "샴페인" : 1
             presentEventResult.put(CHAMPAGNE_PRESENT_NAME, present.get(CHAMPAGNE_PRESENT_NAME));
             // <혜택 내역> 업데이트 "증정 이벤트" : 25000
@@ -31,4 +33,22 @@ public class PresentEvent {
         return presentEventResult;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : present.entrySet()) {
+            String itemName = entry.getKey();
+            int quantity = entry.getValue();
+            result.append(itemName)
+                    .append(SPACE)
+                    .append(quantity)
+                    .append(COUNT_STRING)
+                    .append(NEXT_LINE);
+        }
+        if (result.length() == 0){
+            result.append(NON_VALUE)
+                    .append(NEXT_LINE);
+        }
+        return result.toString();
+    }
 }
