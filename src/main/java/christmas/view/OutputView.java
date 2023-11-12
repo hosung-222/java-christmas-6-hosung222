37ad97c43857e1709fc1953747d1f25d1e0510c0
hosung-222 <gik11kr@naver.com>
2023-11-12 12:00:34 +0900

    feat : 할인 전 총 주문 금액 출력 구현
    할인 전 총 주문 금액에 대해 콤마(,)로 포맷한 원(won)을 출력하는 기능

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -5,6 +5,9 @@ import christmas.domain.Order;
 public class OutputView {
 
     private static final String ORDER_MENU_TITLE = "<주문 메뉴>";
+    private static final String BEFORE_SALE_PRICE_TITLE = "<할인 전 총주문 금액>";
+    private static final String KOREA_WON = "원";
+
 
     public static void printErrorMessageFor(IllegalArgumentException exception) {
         System.out.println(exception.getMessage());
@@ -15,4 +18,10 @@ public class OutputView {
         System.out.println(order.toString());
     }
 
+    public static void printBeforeSalePrice(Order order){
+        System.out.println(BEFORE_SALE_PRICE_TITLE);
+        StringBuilder result = new StringBuilder();
+        result.append(String.format("%,d", order.getTotalPrice())).append(KOREA_WON);
+        System.out.println(result);
+    }
 }
