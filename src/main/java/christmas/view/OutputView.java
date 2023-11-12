40ee5bdbefd669185bc4c14aec3ae46d87c4477d
hosung-222 <gik11kr@naver.com>
2023-11-12 11:44:18 +0900

    feat : 주문된 메뉴를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,7 +1,18 @@
 package christmas.view;
 
+import christmas.domain.Order;
+
 public class OutputView {
+
+    private static final String ORDER_MENU_TITLE = "<주문 메뉴>";
+
     public static void printErrorMessageFor(IllegalArgumentException exception) {
         System.out.println(exception.getMessage());
     }
+
+    public static void printOrderList(Order order){
+        System.out.println(ORDER_MENU_TITLE);
+        System.out.println(order.toString());
+    }
+
 }
