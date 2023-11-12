9869d01cc03b1948846efa68df8417d9ed5a68e5
hosung-222 <gik11kr@naver.com>
2023-11-12 14:15:37 +0900

    feat : 할인 금액 목록 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,12 +1,16 @@
 package christmas.view;
 
 import christmas.domain.Order;
+import christmas.domain.TotalEvent;
 
 public class OutputView {
 
     private static final String ORDER_MENU_TITLE = "<주문 메뉴>";
     private static final String BEFORE_SALE_PRICE_TITLE = "<할인 전 총주문 금액>";
     private static final String KOREA_WON = "원";
+    private static final String EVENT_LIST_TITLE = "<혜택 내역>";
+
+    private static final String NEW_LINE = "\n";
 
 
     public static void printErrorMessageFor(IllegalArgumentException exception) {
@@ -21,7 +25,12 @@ public class OutputView {
     public static void printBeforeSalePrice(Order order){
         System.out.println(BEFORE_SALE_PRICE_TITLE);
         StringBuilder result = new StringBuilder();
-        result.append(String.format("%,d", order.getTotalPrice())).append(KOREA_WON);
+        result.append(String.format("%,d", order.getTotalPrice())).append(KOREA_WON).append(NEW_LINE);
         System.out.println(result);
+    }
+
+    public static void printEventList(TotalEvent totalEvent){
+        System.out.println(EVENT_LIST_TITLE);
+        System.out.println(totalEvent.toString());
     }
 }
